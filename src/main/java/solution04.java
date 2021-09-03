4dd65572d0e9c24a02535154087caaf8f8b162c0
Jeanne Moore <mooresea910@gmail.com>
2021-09-03 11:22:39 -0400

    Initial commit

---
+++ src/main/java/solution04.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class solution04 {
+    /*
+        Create an application that receives input words to form a MadLib-sentence,
+        take in noun
+        take in verb
+        take in adjective that applies to the noun
+        take in adverb that applies to the verb
+        Output a sentence using a single output statement using all 4 input strings
+        Output example: "Do you <verb> your <adjective> <noun> <adverb>?"
+     */
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String noun, verb, adj, adv;
+        System.out.printf("Enter a noun: ");
+        noun = input.nextLine();
+        System.out.printf("Enter a verb: ");
+        verb = input.nextLine();
+        System.out.printf("Enter an adjective: ");
+        adj = input.nextLine();
+        System.out.printf("Enter an adverb: ");
+        adv = input.nextLine();
+
+        System.out.printf("Do you %s your %s %s %s?",verb,adj,noun,adv);
+    }
+
+
+
+
+
+
+}
